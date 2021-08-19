95b0f7db5500a66dbe9b56e3a6a89a3099c683af
gerold-csendes-epam <Gerold_Csendes@epam.com>
2021-08-19 21:42:35 +0200

    cleanup

--- tutorial.py
+++ tutorial.py
@@ -1,11 +1,12 @@
 import json
 import praw
+import time
 from pprint import pprint
 
 import boto3
 import uuid
 
-client = boto3.client('kinesis', region_name='us-east1')
+client = boto3.client('kinesis', region_name='us-east-1')
 partition_key = str(uuid.uuid4())
 
 if __name__ == "__main__":
@@ -15,33 +16,38 @@ if __name__ == "__main__":
     subreddit = reddit.subreddit("wallstreetbets")
     print(subreddit.display_name)
 
-    num_posts = 20
+    num_posts = 100
 
     for i in range(num_posts):
 
         submission = reddit.subreddit("wallstreetbets").random()
         # submission = reddit.submission(id="p0j8bg")
         print(submission.title)
-        # print(submission.id)
-        # print(submission.url)
+        print(submission.id)
+        print(submission.url)
 
-        print(type(submission))
-        submission.comments.replace_more(limit=30)
+        # print(type(submission))
+        submission.comments.replace_more(limit=50)
         # pprint(vars(submission))
-        # pprint(submission.selftext)
+
         for cnt, comment in enumerate(submission.comments.list()):
-            print(type(comment))
-            # print(comment.body)
-            # pprint(vars(comment))
-            print(type(vars(comment)))
+            pprint(vars(comment))
             comment = vars(comment)
             comment = {key: comment[key] for key in ['body', 'score']}
-            print(comment)
+            comment['permalink'] = submission.permalink
+            comment['submission_id'] = submission.id
+
+            pprint(comment)
             data = json.dumps(comment)
 
-            client.put_record(
-                StreamName='wallstreetbets',
-                Data=data,
-                PartitionKey=partition_key)
+            # client.put_record(
+            #     StreamName='wallstreetbets',
+            #     Data=data,
+            #     PartitionKey=partition_key)
+
+            time.sleep(1)
+            break
+
+        break
 
         print(cnt)
